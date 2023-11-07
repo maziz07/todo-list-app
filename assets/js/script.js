689b2b940e119099bd4abdc6480f50aac4882274
maziz07 <102008721@student.swin.edu.au>
2023-11-07 11:40:25 +1100

    updated script and styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
 console.log('script.js is linked correctly');
+document.getElementById('add-task').addEventListener('click', function() {
+    var taskValue = document.getElementById('new-task').value.trim();
+
+    if (taskValue) {
+        var li = document.createElement('li');
+        li.textContent = taskValue;
+        li.addEventListener('click', function() {
+            li.classList.toggle('completed');
+        });
+        document.getElementById('task-list').appendChild(li);
+        document.getElementById('new-task').value = '';
+    }
+});

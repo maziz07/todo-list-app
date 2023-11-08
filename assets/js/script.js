2658d62ca6ff303dd0b93561ad76b9bcd5e1b77b
maziz07 <102008721@student.swin.edu.au>
2023-11-08 14:42:21 +1100

    added new delete feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,74 @@
-console.log('script.js is linked correctly');
+function saveTasks() {
+    var tasks = [];
+    document.querySelectorAll('#task-list li').forEach(function(taskItem) {
+        tasks.push({
+            text: taskItem.firstChild.textContent,
+            completed: taskItem.classList.contains('completed')
+        });
+    });
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function loadTasks() {
+    var tasks = JSON.parse(localStorage.getItem('tasks'));
+    if (tasks) {
+        tasks.forEach(function(task) {
+            var li = document.createElement('li');
+            var deleteBtn = document.createElement('button');
+            
+            deleteBtn.textContent = 'Delete';
+            deleteBtn.className = 'delete-task';
+            deleteBtn.onclick = function() {
+                li.remove();
+                saveTasks(); // Update storage after deletion
+            };
+
+            li.textContent = task.text;
+            li.appendChild(deleteBtn);
+            if (task.completed) {
+                li.classList.add('completed');
+            }
+
+            li.addEventListener('click', function(e) {
+                if (e.target !== deleteBtn) {
+                    li.classList.toggle('completed');
+                    saveTasks(); // Update storage after toggling completion
+                }
+            });
+
+            document.getElementById('task-list').appendChild(li);
+        });
+    }
+}
+
+// Call loadTasks to populate the list on page load
+loadTasks();
+
 document.getElementById('add-task').addEventListener('click', function() {
     var taskValue = document.getElementById('new-task').value.trim();
 
     if (taskValue) {
         var li = document.createElement('li');
+        var deleteBtn = document.createElement('button');
+        
+        deleteBtn.textContent = 'Delete';
+        deleteBtn.className = 'delete-task';
+        deleteBtn.onclick = function() {
+            li.remove();
+        };
+
         li.textContent = taskValue;
-        li.addEventListener('click', function() {
-            li.classList.toggle('completed');
+        li.appendChild(deleteBtn);
+
+        li.addEventListener('click', function(e) {
+            // Prevent the list item from being marked as complete when the delete button is clicked
+            if (e.target !== deleteBtn) {
+                li.classList.toggle('completed');
+            }
         });
+
         document.getElementById('task-list').appendChild(li);
         document.getElementById('new-task').value = '';
     }
+    saveTasks();
 });
